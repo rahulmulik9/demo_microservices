6663e95abbe57a657e58e8789d2a40eb2d9f5da8
RAHUL <rahulmulik9@gmail.com>
2025-01-23 20:20:13 +0530

    Initial Committee

---
+++ services-registry/src/test/java/com/rahul/services_registry/ServicesRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.services_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
