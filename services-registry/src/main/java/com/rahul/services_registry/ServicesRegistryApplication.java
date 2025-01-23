0ca92be3517e95fe96c9e632ac0b1cadbd203d13
RAHUL <rahulmulik9@gmail.com>
2025-01-23 20:36:34 +0530

    Added configuration in application file

--- services-registry/src/main/java/com/rahul/services_registry/ServicesRegistryApplication.java
+++ services-registry/src/main/java/com/rahul/services_registry/ServicesRegistryApplication.java
@@ -2,8 +2,10 @@ package com.rahul.services_registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServicesRegistryApplication {
 
 	public static void main(String[] args) {
