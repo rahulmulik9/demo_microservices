cb17d7cbece96d5f32bee7a688d4687efd7481cf
RAHUL <rahulmulik9@gmail.com>
2025-01-24 20:55:19 +0530

    Added Department.java DepartmentController.java DepartmentRepository.java

---
+++ department-service/src/main/java/com/rahul/department_service/controller/DepartmentController.java
@@ -0,0 +1,9 @@
+package com.rahul.department_service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/department")
+public class DepartmentController {
+}
---
+++ department-service/src/main/java/com/rahul/department_service/repository/DepartmentRepository.java
@@ -0,0 +1,7 @@
+package com.rahul.department_service.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DepartmentRepository {
+}
