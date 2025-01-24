5bebc5e2f9e976fdcaca89f480f8b0224c646787
RAHUL <rahulmulik9@gmail.com>
2025-01-24 21:03:55 +0530

    Added API in DepartmentController.java

--- department-service/src/main/java/com/rahul/department_service/controller/DepartmentController.java
+++ department-service/src/main/java/com/rahul/department_service/controller/DepartmentController.java
@@ -1,9 +1,35 @@
 package com.rahul.department_service.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.rahul.department_service.model.Department;
+import com.rahul.department_service.repository.DepartmentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/department")
 public class DepartmentController {
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+
+    @PostMapping
+    public Department add(@RequestBody Department theDepartment){
+        return departmentRepository.addDepartment(theDepartment);
+    }
+
+    @GetMapping
+    public List<Department> findAll(){
+        return departmentRepository.findAll();
+    }
+
+    @GetMapping
+    public Department findById(@PathVariable Long id){
+        return departmentRepository.findById(id);
+    }
+
 }
